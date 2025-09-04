dd10bca970f387a9b5d3d96f260d1e464368b2f2
Simo272003 <136693053+Simo272003@users.noreply.github.com>
2025-09-04 16:07:54 +0200

    Update Society_route.js

--- backend-server/src/routes/Society_route.js
+++ backend-server/src/routes/Society_route.js
@@ -5,7 +5,7 @@ const  societyController  = require("../controllers/society_controller/society_c
 const { tokenValidation, roleValidation } = require("../controllers/auth_controller/authController");
 
 router.get("/getSocietyData", tokenValidation, roleValidation(['president']), societyController.getSocietyData)
-router.post("/sendSocietyData", tokenValidation, roleValidation(['president']), societyController.sendSocietyData)
+router.post("/sendSocietyData", societyController.sendSocietyData)
 router.put("/updateSocietyData", tokenValidation, roleValidation(['president']), societyController.updateSocietyData)
 
 module.exports = router;
