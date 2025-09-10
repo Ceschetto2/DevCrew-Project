0988cd4f2c33900c634bdca1acb4bf1db0050892
Claudev <c.cappelluti2@studenti.uniba.it>
2025-09-10 23:32:14 +0200

    Update Loading.jsx

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -4,11 +4,11 @@ export function  Loading  ({ isLoading })  {
   return (
     <div>
       {isLoading && (
-        <div className="flex items-center justify-center mt-2">
-          <div className="spinner-border text-blue-500" role="status">
+        
+
             <span className="visually-hidden">Loading...</span>
-          </div>
-        </div>
+         
+     
       )}
     </div>
   );
